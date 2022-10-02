544bcad275c767db41d65f72ba2f52fc1f22de9f
Oussama Joubouti <ojoubouti@leyton.com>
2022-10-02 16:33:45 +0100

    finalize part 1

--- part-1/scripts/server.sh
+++ part-1/scripts/server.sh
@@ -1,4 +1,11 @@
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+# cd /etc/yum.repos.d/
+# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# systemctl start firewalld
+# systemctl enable firewalld
+
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -
 cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/
 
 firewall-cmd --zone=public --permanent --add-port 6443/tcp
--- part-1/scripts/worker.sh
+++ part-1/scripts/worker.sh
@@ -1,1 +1,5 @@
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.42.110:6443 K3S_TOKEN_FILE=/vagrant/confs/node-token sh -
+# cd /etc/yum.repos.d/
+# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_URL=https://192.168.42.110:6443 K3S_TOKEN_FILE=/vagrant/confs/node-token sh -
