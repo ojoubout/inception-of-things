91eb446b7e84870475e78a56be89071a66fa3ab2
Oussama Joubouti <ojoubouti@leyton.com>
2022-10-02 16:35:47 +0100

    remove confs folder

--- part-1/scripts/server.sh
+++ part-1/scripts/server.sh
@@ -6,7 +6,7 @@
 
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -
-cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/
+cp /var/lib/rancher/k3s/server/node-token /vagrant/
 
 firewall-cmd --zone=public --permanent --add-port 6443/tcp
 firewall-cmd --reload
--- part-1/scripts/worker.sh
+++ part-1/scripts/worker.sh
@@ -2,4 +2,4 @@
 # sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 # sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_URL=https://192.168.42.110:6443 K3S_TOKEN_FILE=/vagrant/confs/node-token sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_URL=https://192.168.42.110:6443 K3S_TOKEN_FILE=/vagrant/node-token sh -
